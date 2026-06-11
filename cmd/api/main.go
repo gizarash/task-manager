4eb048424e5dc8ebfe8ce54f962a770d9ffe68ed
Rashid Gizatullin <rashidg@halykbank.kz>
2026-06-11 09:22:28 +0500

    done fourth task

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/gizarash/task-manager/internal/model"
 	"github.com/gizarash/task-manager/internal/store"
@@ -124,9 +129,60 @@ func main() {
 		}
 	})
 
-	slog.Info("starting server at port 8080...")
-	err = http.ListenAndServe(":8080", mux)
-	if err != nil {
-		slog.Error("unable to start server", "error", err)
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
+	
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		slog.Info("starting server at port 8080...")
+		if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("unable to start server", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				todos := store.List()
+				done := 0
+				pending := 0
+
+				for _, todo := range todos {
+					if todo.Done {
+						done++
+					} else {
+						pending++
+					}
+				}
+
+				slog.Info(fmt.Sprintf("stats: total=%d, done=%d, pending=%d", len(todos), done, pending))
+			}
+		}
+	}(ctx)
+
+	<-ctx.Done()
+	slog.Info("server is shutting down...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(shutdownCtx); err != nil {
+		slog.Error(fmt.Sprintf("server forced to shutdown: %s", err))
+	}
+
+	slog.Info("server stopped")
 }
